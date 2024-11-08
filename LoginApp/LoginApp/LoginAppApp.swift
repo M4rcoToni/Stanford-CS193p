5844d048adc89cdade9dd3a65001914c59a3f7c5
m4rcotoni <marcosanto.sl021@academico.ifsul.edu.br>
2024-11-08 00:04:07 -0300

    register and login with firebase

---
+++ LoginApp/LoginApp/LoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAppApp.swift
+//  LoginApp
+//
+//  Created by marco on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
